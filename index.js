34ef5712907561fe3f463fea4bce776f450a098d
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2024-04-21 15:32:04 -0300

    testando

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ passport.use(
         try {
             let user = await userExists(profile.emails[0].value);
 
-            if(typeof(user) == 'undefined') {
+            if(typeof(user) === 'undefined') {
+                console.log('oii');
                 user = new User({
                     idUser: profile.id,
                     name: profile.displayName,
@@ -68,6 +69,8 @@ passport.use(
                     photo: profile.photos[0].value
                 })
 
+                console.log(user.name);
+                
                 await database.create(user.name, user.email, user.password, user.photo).then(() => {
                     return done(null, profile);
                 });           
