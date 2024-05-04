070814b3853494fb9ac7751326b13cd3959ffcc0
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2024-05-04 17:44:12 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 import {sql} from './database-connect.js';
 
 export class Database {
-    //retornar todos os usuários do banco
     async getUsers() {
         const users = await sql`select * from users`;
         return users;
@@ -11,8 +10,15 @@ export class Database {
         await sql `update users set password = ${password} where email = ${email}`;
     }
 
-    async create(name, email, password, photo) {
-        console.log(name)
-        await sql `insert into users (name, email, password, photo) values (${name}, ${email}, ${password}, ${photo})`;	
+    async create(user) {
+        await sql `insert into users (name, email, password, photo) values (${user.name}, ${user.email}, ${user.password}, ${user.photo})`;	
+    }
+
+    async createPost(post) {
+        await sql `insert into posts (id_user, content, timePost, nameBook, imageBook) values (${post.idUser}, ${post.content}, ${post.timePost}, ${post.nameBook}, ${post.imageBook})`;
+    }
+
+    async createReview(review) {
+        await sql `insert into reviews (id_user, title, content, nameBook, rating, image, time_post) values (${review.idUser},${review.title}, ${review.content}, ${review.nameBook}, ${review.rating}, ${review.imageBook}, ${review.timePost})`;
     }
 }
