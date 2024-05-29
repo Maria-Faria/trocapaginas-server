3e6157fb198f0e75152abe364e8fe15eb671a92f
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2024-05-28 23:47:29 -0300

    adjusting limit

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ app.use(cors({
   }
 ));
 
-app.use(express.bodyParser({limit: '50mb'}))
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 app.use(session({
   secret: process.env.SECRET,
@@ -102,19 +101,11 @@ app.get('/login-google', (req, res) => {
 
 app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-/*app.get('/login/google', (req, res) => {
-  res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=https://trocapaginas-server-production.up.railway.app/auth/google/callback&scope=profile%20email&client_id=${clientid}`);
-});*/
 
 app.get('/auth/google/callback', passport.authenticate('google', {
   successRedirect: '/success',
   failureRedirect: '/login/failed'
 }));
-
-/*app.get('/auth/google/callback', (req, res) => {
-  // Lide com a resposta do Google aqui
-  console.log(res.data);
-});*/
 
 app.get('/success', (req, res) => {
   const htmlResponse = `
