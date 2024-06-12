372e51b264b72379022283d0ae27f8fbe66f7d44
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2024-06-12 19:39:55 -0300

    test image post 2

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -31,7 +31,7 @@ async function userExists(email) {
 
 function validateImage(imageURI){
     console.log(imageURI)
-    if(imageURI === undefined) {
+    if(imageURI === undefined || imageURI === '') {
         review.imageBook = 'https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-05-256.png';
         post.imageBook = 'https://cdn2.iconfinder.com/data/icons/new-year-resolutions/64/resolutions-05-256.png';
 
