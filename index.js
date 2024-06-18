079dc9909b7164c1af5df18767a5918aad5eae49
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2024-06-18 16:25:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -163,8 +163,8 @@ app.get('/user-data', async (req, res) => {
 });
 
 
-app.listen(() => { 
-  console.log(`Servidor rodando`);
+app.listen(port() => { 
+  console.log(`Servidor rodando na porta ${port}`);
 });
 
 app.use(routes);
